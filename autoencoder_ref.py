3ad3434026a81fbc1cceb8b0d859bfb4483aa341
Anna Shors <ashors@stanford.edu>
2021-02-01 23:41:03 -0500

    removed unnecessary helper functions

--- autoencoder_ref.py
+++ autoencoder_ref.py
@@ -1,35 +1,10 @@
 import sys
 import numpy as np
-from typing import Tuple
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import tensorflow_datasets as tfds
 
 MAX_SEQUENCE_LENGTH = 29
-TRAIN_URL = None
-
-def load_file(file_path: str) -> Tuple[Tuple[str], Tuple[str]]:
-    """ A helper functions that loads the file into a tuple of strings
-
-    :param file_path: path to the data file
-    :return factors: (LHS) inputs to the model
-            expansions: (RHS) group truth
-    """
-    data = open(file_path, "r").readlines()
-    factors, expansions = zip(*[line.strip().split("=") for line in data])
-    return factors, expansions
-
-
-def score(true_expansion: str, pred_expansion: str) -> int:
-    """ the scoring function - this is how the model will be evaluated
-
-    :param true_expansion: group truth string
-    :param pred_expansion: predicted string
-    :return:
-    """
-    return int(true_expansion == pred_expansion)
-
 
 def tokenize(lang):
     lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
