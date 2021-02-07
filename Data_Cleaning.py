4c74ec79a7b57aa3c12e15996a50a1cd0f298b05
aefreeman <andrew@DESKTOP-7U6JKBJ.localdomain>
2021-02-07 14:18:48 -0800

    Initial txt_clean

---
+++ Data_Cleaning.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 12:15:36 2021
+
+@author: Andre
+"""
+
+import pandas as pd
+import numpy as np
+import re
+
+df = pd.read_csv('wiki_movie_plots_deduped.csv')
+#df = df.loc[df.Title =="What the Daisy Said"]
+df = df[['Plot']]
+#Don't match Initial. or Mr/Jr/.../"..." or Mrs/ie/eg AND 
+#   don't match future punctuation (i.e. the first two dots in an ellipsis)
+match_str = '(?<!\.)(?<![A-Z])(?<!Mr|Jr|Sr|Fr|Ms|Mz)(?<!Mrs|i\.e|e\.g)[\.|?|!](?!\.)'
+min_sent = 5
+sents = [x for y in df.Plot.str.split(match_str).tolist() for x in y]
+#Match [#] or (_anything_)
+bracket_match = '(\[[0-9]+\])|(\([^\)]*\))'
+sents = [re.sub(bracket_match,'',x) for x in sents]
+sents = [x.lstrip().rstrip() for x in sents if len(x) > min_sent]
+df_new = pd.DataFrame(sents, columns = ['Sentences'])
+
+df_new.to_csv(r'Sentences.txt', header=None, index=None, sep=' ', mode='w')
