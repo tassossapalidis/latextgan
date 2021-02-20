b1883e17d37aaf43ee25549968624d590bf8212b
Anna Shors <ashors@stanford.edu>
2021-02-19 20:38:40 -0500

    gan training udpates

--- gan.py
+++ gan.py
@@ -9,16 +9,16 @@ class ResNetBlock(keras.Model):
         super().__init__()
 
         # linear layer with ReLU activation function
-        self.layer1 = keras.layers.Dense(block_dim, activation = 'relu')
+        self.layer1 = keras.layers.Dense(block_dim, activation = 'relu', kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None))
         # linear layer
-        self.layer2 = keras.layers.Dense(block_dim)
+        self.layer2 = keras.layers.Dense(block_dim, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None))
     
     def call(self, x):
         # ResNet: input is added to layer output
         x = self.layer2(self.layer1((x))) + x
         return x
 
-class Generator(keras.Sequential):
+'''class Generator(keras.Sequential):
     def __init__(self, n_layers, block_dim):
         super(Generator, self).__init__()
 
@@ -34,7 +34,40 @@ class Discriminator(keras.Sequential):
         for i in range(n_layers):
             self.add(ResNetBlock(block_dim))
         
-        self.add(keras.layers.Dense(1, activation='linear'))
+        #self.add(keras.layers.Dense(1, activation='linear'))
+        self.add(keras.layers.Dense(1, activation='sigmoid'))
+'''
+
+class Generator(keras.Model):
+    def __init__(self, n_layers, block_dim):
+        super(Generator, self).__init__()
+
+        self.n_layers = n_layers
+        self.res_blocks = [ResNetBlock(block_dim) for _ in range(n_layers)]
+
+    def call(self, x):
+        for i in range(self.n_layers):
+            x = self.res_blocks[i](x)
+        return x
+
+class Discriminator(keras.Model):
+    def __init__(self, n_layers, block_dim):
+        super(Discriminator, self).__init__()
+
+        self.n_layers = n_layers
+        self.res_blocks = [ResNetBlock(block_dim) for _ in range(n_layers)]
+
+        self.ff = keras.layers.Dense(1, activation='linear')
+
+    def call(self, x):
+        for i in range(self.n_layers):
+            x = self.res_blocks[i](x)
+        #print("x before: {}".format(x)) 
+        x = self.ff(x)
+        #print("x after: {}".format(x))
+        x = tf.nn.sigmoid(x)
+        #print("x after sigmoid: {}".format(x))
+        return x
 
 if __name__ == '__main__':
     # create a generator
