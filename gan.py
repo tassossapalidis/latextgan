c337e9c93e00be76522629b99ddd61d1cc7766e0
Tassos Sapalidis <66144841+tassossapalidis@users.noreply.github.com>
2021-02-26 12:22:08 -0800

    edits to gan.py and milestone

--- gan.py
+++ gan.py
@@ -9,29 +9,29 @@ class ResNetBlock(keras.Model):
         super().__init__()
 
         # linear layer with ReLU activation function
-        self.layer1 = keras.layers.Dense(block_dim, activation = 'relu', kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None))
+        self.layer1 = keras.layers.Dense(block_dim, activation = 'relu', kernel_initializer=tf.keras.initializers.HeNormal())
         # linear layer
-        self.layer2 = keras.layers.Dense(block_dim, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None))
+        self.layer2 = keras.layers.Dense(block_dim, kernel_initializer=tf.keras.initializers.HeNormal())
         self.relu = keras.layers.ReLU()
 
     def call(self, x):
         # ResNet: input is added to layer output
         x = self.layer2(self.layer1((x))) + x
-        return self.relu(x)
+        return x
 
 class ResNetLeakyRelu(keras.Model):
     def __init__(self, block_dim):
         super().__init__()
 
         # linear layer with ReLU activation function
-        self.layer1 = keras.layers.Dense(block_dim, activation = "linear", kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None))
+        self.layer1 = keras.layers.Dense(block_dim, activation = "linear", kernel_initializer=tf.keras.initializers.HeNormal())
         self.leaky_relu = tf.keras.layers.LeakyReLU()
-        self.layer2 = keras.layers.Dense(block_dim, kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None))
+        self.layer2 = keras.layers.Dense(block_dim, kernel_initializer=tf.keras.initializers.HeNormal())
         self.relu = keras.layers.ReLU()
 
     def call(self, x):
         x = self.layer2(self.leaky_relu(self.layer1(x))) + x
-        return self.relu(x)
+        return x
 
 class Generator(keras.Model):
     def __init__(self, n_layers, block_dim):
@@ -39,30 +39,28 @@ class Generator(keras.Model):
 
         self.n_layers = n_layers
         self.res_blocks = [ResNetBlock(block_dim) for _ in range(n_layers)]
+        self.relu = keras.layers.ReLU()
 
     def call(self, x):
         for i in range(self.n_layers):
             x = self.res_blocks[i](x)
-        return x
+        return self.relu(x)
 
 class Discriminator(keras.Model):
     def __init__(self, n_layers, block_dim):
         super(Discriminator, self).__init__()
 
         self.n_layers = n_layers
-        #self.res_blocks = [ResNetBlock(block_dim) for _ in range(n_layers)]
         self.res_blocks = [ResNetLeakyRelu(block_dim) for _ in range(n_layers)]
 
+        self.relu = keras.layers.ReLU()
         self.ff = keras.layers.Dense(1, activation='linear')
 
     def call(self, x):
         for i in range(self.n_layers):
             x = self.res_blocks[i](x)
-        #print("x before: {}".format(x)) 
+        x = self.relu(x)
         x = self.ff(x)
-        #print("x after: {}".format(x))
-        #x = tf.nn.sigmoid(x)
-        #print("x after sigmoid: {}".format(x))
         return x
 
 if __name__ == '__main__':
